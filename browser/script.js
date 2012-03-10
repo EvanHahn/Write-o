71c9e1b31aac13328bc0b67a8ff92b5dfff99b07
Evan Hahn <me@evanhahn.com>
2012-03-09 19:39:46 -0500

    Local storage

--- browser/script.js
+++ browser/script.js
@@ -23,6 +23,21 @@ Writeo.fonts = [
 	'Georgia, serif', 'sans-serif', 'monospace'
 ];
 
+// Local storage
+Writeo.local = function(key, value) {
+    // Getter
+	if (arguments.length === 1)
+		return localStorage.getItem(key);
+    // Clear it no matter what (for iOS)
+    localStorage.removeItem(key);
+    // Setter
+    if (value !== null) {
+        if ((typeof value !== 'string') && (!(value instanceof String)))
+            value = JSON.stringify(value);
+        localStorage.setItem(key, value);
+    }
+};
+
 // Important selectors
 Writeo.textarea = document.getElementById('text');
 Writeo.toolbar = document.getElementById('toolbar');
@@ -33,9 +48,11 @@ Writeo.fontsElement = document.getElementById('fonts');
 Writeo.applyTheme = function(theme) {
 	document.body.style.background = theme.background;
 	Writeo.textarea.style.color = theme.color;
+	Writeo.local('theme', theme);
 };
 Writeo.applyFont = function(font) {
 	Writeo.textarea.style.fontFamily = font;
+	Writeo.local('font', font);
 }
 
 // Create toolbar and things
@@ -59,6 +76,28 @@ for (var i = 0; i < fontsLength; i ++) {
 	Writeo.fontsElement.appendChild(li);
 }
 
-// Default
-Writeo.applyTheme(Writeo.themes[0]);
-Writeo.applyFont(Writeo.fonts[0]);
+// Load from storage (or fall back to defaults)
+var localTheme = JSON.parse(Writeo.local('theme'));
+if (localTheme)
+	Writeo.applyTheme(localTheme);
+else
+	Writeo.applyTheme(Writeo.themes[0]);
+var localFont = Writeo.local('font');
+if (localFont)
+	Writeo.applyFont(localFont);
+else
+	Writeo.applyFont(Writeo.fonts[0]);
+
+// Load text from storage
+var localText = Writeo.local('text');
+if (localText)
+	Writeo.textarea.innerHTML = localText;
+
+// Save all the damn time
+Writeo.saveText = function() {
+	Writeo.local('text', Writeo.textarea.value)
+};
+Writeo.textarea.onkeypress = Writeo.saveText;
+Writeo.textarea.onkeydown = Writeo.saveText;
+Writeo.textarea.onkeyup = Writeo.saveText;
+setInterval(Writeo.saveText, 1000);
