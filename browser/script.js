786099b7fd63943e2bd22eea63acdff815d18cdf
Evan Hahn <me@evanhahn.com>
2012-03-09 19:26:39 -0500

    Toolbar works

--- browser/script.js
+++ browser/script.js
@@ -7,12 +7,12 @@ else
 // Configs
 Writeo.themes = [
 	{
-		background: '#000',
-		color: '#fff'
+		background: '#f0f0f0',
+		color: '#000'
 	},
 	{
-		background: '#fff',
-		color: '#000'
+		background: '#000',
+		color: '#fff'
 	},
 	{
 		background: '#fed',
@@ -20,11 +20,14 @@ Writeo.themes = [
 	}
 ];
 Writeo.fonts = [
-	'Inconsolata, Droid Sans Mono, Monaco, monospace', 'sans-serif', 'Georgia, serif'
+	'Georgia, serif', 'sans-serif', 'monospace'
 ];
 
 // Important selectors
 Writeo.textarea = document.getElementById('text');
+Writeo.toolbar = document.getElementById('toolbar');
+Writeo.themesElement = document.getElementById('themes');
+Writeo.fontsElement = document.getElementById('fonts');
 
 // Apply themes and fonts
 Writeo.applyTheme = function(theme) {
@@ -34,3 +37,28 @@ Writeo.applyTheme = function(theme) {
 Writeo.applyFont = function(font) {
 	Writeo.textarea.style.fontFamily = font;
 }
+
+// Create toolbar and things
+var themesLength = Writeo.themes.length;
+for (var i = 0; i < themesLength; i ++) {
+	var li = document.createElement('li');
+	var a = document.createElement('a');
+	a.href = 'javascript:Writeo.applyTheme(Writeo.themes[' + i + ']);';
+	a.style.background = Writeo.themes[i].background;
+	li.appendChild(a);
+	Writeo.themesElement.appendChild(li);
+}
+var fontsLength = Writeo.fonts.length;
+for (var i = 0; i < fontsLength; i ++) {
+	var li = document.createElement('li');
+	var a = document.createElement('a');
+	a.href = 'javascript:Writeo.applyFont(Writeo.fonts[' + i + ']);';
+	a.style.fontFamily = Writeo.fonts[i];
+	a.innerHTML = 'W';
+	li.appendChild(a);
+	Writeo.fontsElement.appendChild(li);
+}
+
+// Default
+Writeo.applyTheme(Writeo.themes[0]);
+Writeo.applyFont(Writeo.fonts[0]);
